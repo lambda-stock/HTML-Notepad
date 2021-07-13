bfb437428ce11507a059526308fa62b7ec3a40bd
yigid-lambda <58011517+yigid-lambda@users.noreply.github.com>
2021-07-13 13:29:41 +0300

    Added script.js file

---
+++ script.js
@@ -0,0 +1,16 @@
+function download() {
+        var text = document.getElementById("notes").value;
+        var filename = document.getElementById("filename").value;
+        text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
+        var blob = new Blob([text], {
+            type: "text/plain"
+        });
+        var anchor = document.createElement("a");
+        anchor.download = filename + ".txt";
+        anchor.href = window.URL.createObjectURL(blob);
+        anchor.target = "_blank";
+        anchor.style.display = "none"; // just to be safe!
+        document.body.appendChild(anchor);
+        anchor.click();
+        document.body.removeChild(anchor);
+}
